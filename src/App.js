b80b26e432c3f2a7cbb68f63e34aef5ae58d0e15
HamzaSultan <56472325+HamzaSultan@users.noreply.github.com>
2020-06-18 22:40:15 +0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br>Edited By Ameer Hamza</br>
+          <br>Edited By Ameer Hamza Sultan</br>
         </p>
         <a
           className="App-link"
