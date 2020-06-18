e3c249da7251da82910e995a239d1f0ba98a8d1f
HamzaSultan <56472325+HamzaSultan@users.noreply.github.com>
2020-06-18 22:41:23 +0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <br>Edited By Ameer Hamza</br>
           <br>Edited By Ameer Hamza Sultan</br>
         </p>
         <a
